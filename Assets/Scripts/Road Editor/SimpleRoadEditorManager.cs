6d8a2289847fbbf8602f147dc0a2ca570fa59226
Nitsan Caduri <nitsan447@gmail.com>
2024-10-16 13:30:46 +0300

    First Commit

---
+++ Assets/Scripts/Road Editor/SimpleRoadEditorManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRoadEditorManager : RoadEditorManager_Base
+{
+    public override bool Init()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void StartRoadEdit()
+    {
+        throw new System.NotImplementedException();
+    }
+}
