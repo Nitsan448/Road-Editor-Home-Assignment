3ba2384cea2c53114099fc565c8a6e8b01260f74
Nitsan Caduri <nitsan447@gmail.com>
2024-10-16 15:51:32 +0300

    Working on node preview

--- Assets/Scripts/Road Editor/SectionHandler.cs
+++ Assets/Scripts/Road Editor/SectionHandler.cs
@@ -6,6 +6,7 @@ public class SectionHandler
 {
     private GameObject _underConstructionNodePrefab;
     private GameObject _builtNodePrefab;
+    private GameObject _sectionPreviewNode;
 
     public SectionHandler(GameObject underConstructionNodePrefab, GameObject builtNodePrefab)
     {
@@ -15,8 +16,22 @@ public class SectionHandler
 
     public void ShowSectionPreview(Vector3 startPoint, Vector3 endPoint)
     {
+        if (_sectionPreviewNode == null)
+        {
+            _sectionPreviewNode = Object.Instantiate(_underConstructionNodePrefab);
+        }
+
+        SetNodeLength(_sectionPreviewNode, Vector3.Distance(startPoint, endPoint));
+        _sectionPreviewNode.transform.localScale = new Vector3(_sectionPreviewNode.transform.localScale.x,
+            _sectionPreviewNode.transform.localScale.y, Vector3.Distance(startPoint, endPoint));
+        _sectionPreviewNode.transform.position = startPoint;
         Debug.Log(startPoint);
         Debug.Log(endPoint);
+    }
+
+    private void SetNodeLength(GameObject node, float length)
+    {
+        node.transform.localScale = new Vector3(node.transform.localScale.x, node.transform.localScale.y, length);
     }
 
     public void BuildSection(Vector3 startPoint, Vector3 endPoint)
