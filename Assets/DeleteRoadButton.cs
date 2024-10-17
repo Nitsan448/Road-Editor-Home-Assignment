7c9db0fad730e35e67f8cb7c807fd14412a00998
Nitsan Caduri <nitsan447@gmail.com>
2024-10-17 19:00:04 +0300

    Working on deleting roads

--- Assets/DeleteRoadButton.cs
+++ Assets/DeleteRoadButton.cs
@@ -1,13 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DeleteRoadButton : MonoBehaviour
 {
-    private RoadEditorManager_Base _roadEditorManager;
-
     public void Init(RoadEditorManager_Base roadEditorManager)
     {
-        _roadEditorManager = roadEditorManager;
+        GetComponent<Button>().onClick.AddListener(roadEditorManager.DeleteSelectedRoad);
     }
 }
--- Assets/Scripts/Road Editor/RoadBuilder/RoadBuilder.cs
+++ Assets/Scripts/Road Editor/RoadBuilder/RoadBuilder.cs
@@ -59,6 +59,5 @@ public class RoadBuilder
 
     private void DeleteRoad(Junction junctionToDelete)
     {
-
     }
 }
